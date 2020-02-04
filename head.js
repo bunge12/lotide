50cafe5d3cfc3fd40e93ca7093e5d800a35a5ff8
Artur Iatsko <arturyatsko@gmail.com>
2020-02-04 00:14:06 +0000

    Updated test cases

--- head.js
+++ head.js
@@ -13,4 +13,4 @@ function head(array) {
 assertEqual(head([5]), 5);
 assertEqual(head([]), undefined);
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hell", "Lighthouse", "Labs"]), "Hello");

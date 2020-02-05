68e7d5002a1aa3a5731de9ca1c5e46cee7ea471c
Artur Iatsko <arturyatsko@gmail.com>
2020-02-05 22:47:55 +0000

    Letter Positions fixed

---
+++ findKeyByValue.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(list, value) {
+
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
--- letterPositions.js
+++ letterPositions.js
@@ -22,13 +22,14 @@ const eqArrays = function(arr1, arr2) {
 const letterPositions = function(sentence) {
   let result = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] in result) {
-      result[sentence[i]].push(i);
-    } else {
-      result[sentence[i]] = [i];
+    if (sentence[i] !== " ") {
+      if (sentence[i] in result) {
+        result[sentence[i]].push(i);
+      } else {
+        result[sentence[i]] = [i];
+      }
     }
   }
-  console.log(result);
   return result;
 };
 

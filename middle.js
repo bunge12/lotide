733fb29f5b2d30f2b1bc9315b7cf1225daac305f
Artur Iatsko <arturyatsko@gmail.com>
2020-02-05 02:21:59 +0000

    middle implemented

--- middle.js
+++ middle.js
@@ -23,9 +23,13 @@ const middle = function(array) {
   let result = [];
   const length = array.length;
   const midIndex = Math.floor(length / 2);
-  if (length <= 2) {return result;}
-  else if (length % 2 === 0) {result = array.slice(midIndex - 1, midIndex+1);}
-  else { result = array.slice(midIndex, midIndex+1);}
+  if (length <= 2) {
+    return result;
+  } else if (length % 2 === 0) {
+    result = array.slice(midIndex - 1, midIndex + 1);
+  } else {
+    result = array.slice(midIndex, midIndex + 1);
+  }
   return result;
 };
 
---
+++ without.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let y = 0; y < itemsToRemove.length; y++) {
+      if (source[i] === itemsToRemove[y]) {
+        source.splice(i ,1);
+      }
+    }
+  }
+  return source;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"]);
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);

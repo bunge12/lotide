8a6693517b1c49c96cb34b92983c246f4921a27d
Artur Iatsko <arturyatsko@gmail.com>
2020-02-08 00:26:55 +0000

    eqObjects updated recursive

--- eqObjects.js
+++ eqObjects.js
@@ -26,7 +26,10 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+    if (typeof object1[key] === "object" && typeof object2[key] === "object" && !Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+      return eqObjects(object1[key], object2[key]);
+    } 
+    else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key],object2[key]);
     } else if (object1[key] !== object2[key]) {
       return false;
@@ -48,3 +51,10 @@ const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { z: 3 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { z: { qwe: 0} }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { z: { qwe: 0} }, b: 2 }, { a: { z: { qwe: 0} }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);

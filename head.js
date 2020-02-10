75c41362d3041180e6690fa2798dc31a14562417
Artur Iatsko <arturyatsko@gmail.com>
2020-02-10 20:59:38 +0000

    refactor complete for 2 functions

--- head.js
+++ head.js
@@ -4,7 +4,4 @@ const head = (array) => {
   return array[0];
 };
 
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hell", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
